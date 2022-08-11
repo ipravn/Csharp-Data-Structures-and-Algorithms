314121b4ba2672feaea90e0a89642ecc3d0708b7
Praveen Kumar <46933956+ipravn@users.noreply.github.com>
2022-08-11 16:58:45 +0530

    Update Bubble Sort Algorithm.cs

--- Bubble Sort Algorithm.cs
+++ Bubble Sort Algorithm.cs
@@ -31,3 +31,23 @@ namespace Program
         }
     }
 }
+///////////////////////////////////////////////////////////////////////////
+
+public static int[] BubbleSort(int[] arr)
+        {
+            for (int i = 0; i< arr.Length-1 ; i++)
+            {
+                for (int j = 0; j < arr.Length-1; j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+
+            return arr;
+
+        }
